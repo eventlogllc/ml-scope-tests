984f41bc9a878280901d735b2b4b4a8dd496cd4d
eventlogllc <100497634+eventlogllc@users.noreply.github.com>
2022-06-17 08:30:59 -0700

    Update nn.py

--- tests/scope/nn.py
+++ tests/scope/nn.py
@@ -1,11 +1,24 @@
+import unittest
 from unittest.mock import patch
 
 @patch('si.nn')
 @patch('si.ds')
-def test(MockNN, MockDS):
+
+
+class SI_test(unittest.TestCase):
+  def test(MockNN, MockDS):
      si.nn()
      si.ds()
      assert MockNN is si.nn
      assert MockDS is si.ds
      assert MockNN.called
      assert MockDS.called
+
+def main():
+  suite = unittest.TestSuite()
+  suite.addTest(SI_test('test'))
+  unittest.TextTestRunner(verbosity=2).run(suite)
+
+if __name__ == "__main__":
+  unittest.main()
+
