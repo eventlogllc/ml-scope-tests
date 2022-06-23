87c6069fd07f3fc3b6569ff5e4900bec13832779
eventlogllc <100497634+eventlogllc@users.noreply.github.com>
2022-06-23 09:26:12 -0700

    Update pline.py

--- tests/scope/pline.py
+++ tests/scope/pline.py
@@ -12,10 +12,20 @@ from unittest.mock import patch
 ### how 
 ### Validating input data.
     ### [why] low-quality data will significantly affect the model's performance, 
-    ###       tt's much easier to detect low-quality data at input THAN guessing at its existence after the model predicts badly.
-    ### [rules] that the data must satisfy. these rules are the data-schema.
+    ###       it's much easier to detect low-quality data at input THAN guessing at its existence after the model predicts badly.
+    ### [how] rules that the data must satisfy. these rules are the data-schema.
      
-### Validating feature engineering.
+### Validating feature engineering. also called engineered-data
+    ### [why] while the raw data might be valid, the model only sees engineered feature data. 
+    ###       because engineered data looks very different from raw input data, need to check engineered data separately.
+    ### [how] unit tests following
+    ### All numeric features are scaled, for example, between 0 and 1.
+    ### One-hot encoded vectors only contain a single 1 and N-1 zeroes.
+    ### Missing data is replaced by mean or default values.
+    ### Data distributions after transformation conform to expectations. 
+    ###   For example, if you've normalized using z-scores, the mean of the z-scores is 0.
+    ### Outliers are handled, such as by scaling or clipping.
+
 ### Validating quality of new model versions.
 ### Validating serving infrastructure.
 ### Testing integration between pipeline components.
