37612a3850b6c9c3cf3bc9319c4ce9dab5d6501f
eventlogllc <100497634+eventlogllc@users.noreply.github.com>
2022-07-23 08:55:55 -0700

    Update nn.py

--- tests/scope/nn.py
+++ tests/scope/nn.py
@@ -21,6 +21,41 @@ class SI_test(unittest.TestCase):
         assert MockDS is si.ds
         assert MockNN.called
         assert MockDS.called      
+        
+  def test_side_effect(MockNN, MockDS):
+     si.nn()
+     si.ds()
+     
+     out = StringIO.StringIO()
+     err = StringIO.StringIO() 
+     ### 
+     def _func_get_data_side_effect(url, offset):
+        if offset == 1:
+           return { 2, 3 }
+        else:
+           return { -2, -3 }
+          
+     def _func_randome_side_effect(nodes, offset):
+        if offset == 1:
+           return nodes[0:1]
+        else:
+           return nodes[2:3]
+          
+     ### clear the stdout and stderr buffer
+     out.flush()
+     err.flush()
+     with redirect_stdout(f) and redirect_stderr(f):
+        output = out.getvalue()
+        assert MockNN is si.nn
+        assert MockDS is si.ds
+        assert MockNN.called
+        assert MockDS.called     
+        ### use patch side_effect in multiple functions
+        with patch.muptile("s1.nn",
+                   _get_data = _func_get_data_side_effect,
+                   _random = _func_randome_side_effect
+                          )
+                           
 
 def main():
   suite = unittest.TestSuite()
